c779a1f98849b5798157d3c64e19c18fd8a7cf33
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-05-07 21:08:19 +0600

    done

---
+++ src/components/Update.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Update = () => {
+    const loadedUser =useLoaderData()
+
+    const handleUpdate = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value
+        const email = form.email.value
+        console.log(name, email)
+        const updatedUser = {name, email}
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then (res => res.json())
+        .then(data =>{
+            console.log(data);
+            if(data.modifiedCount >0){
+                alert('gg ez updated successfully')
+            }
+        })
+    }
+
+    return (
+        <div>
+            <h3>update information of {loadedUser.name}</h3>
+            <form onSubmit={handleUpdate}>
+                <input type="text" name="name" defaultValue={loadedUser?.name} id="" /> <br />
+                <input type="email" name="email" defaultValue={loadedUser?.email} id="" /> <br />
+                <input type="submit" value="Update" />
+            </form>
+        </div>
+    );
+};
+
+export default Update;
